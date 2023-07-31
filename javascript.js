f712927b1cb0a6b0391dbe0853a5ff5e42bf6298
OatAmOats <vicente.i.tapia@gmail.com>
2023-07-31 10:56:32 -0400

    reorganized for simplicity

--- javascript.js
+++ javascript.js
@@ -1,9 +1,6 @@
-let numOne = document.querySelector('.numOne');
-let numTwo = document.querySelector('.numTwo');
-let op = document.querySelector('.op');
-let disTop = document.querySelector('.disTop');
-
-let op_activated = false;
+//values
+let userInput = document.querySelector('.userInput')
+let results = document.querySelector('.results')
 
 const one = document.querySelector('.one');
 const two = document.querySelector('.two');
@@ -48,158 +45,85 @@ divide = function(a,b){
     }
 }
 
-operate = function(a, operator, b){
-    if(operator === " + "){
-        console.log("hmmmmmm")
+operate = function(ui){
+    const inputArray = ui.split(" ");
+    let a = inputArray[0];
+    let b = inputArray[2];
+    if(inputArray[1] == "+"){
         return(add(a,b));
     }
-    if(operator == " - "){
+    if(inputArray[1] == "-"){
         return(subtract(a,b));
     }
-    if(operator == " × "){
+    if(inputArray[1] == "×"){
         return(multiply(a,b));
     }
-    if(operator == " ÷ "){
+    if(inputArray[1] == "÷"){
         return(divide(a,b));
     }
 }
 
-numWrite = function(num, add){
-    num = num * 10;
-    num = num + add;
-    return(num);
+numWrite = function(num){
+    userInput.textContent = userInput.textContent + String(num);
 }
 
-opActivate = function(operator){
-    op.textContent = operator;
-    op_activated = true;
-}
-
-calc = function(a, what, b){
-    disTop.textContent = `${a} ${what} ${b}`;
-    numOne.textContent = operate(a, what, b);
-    op.textContent = '';
-    numTwo.textContent = '';
-    op_activated = false;
+calc = function(ui){
+    results.textContent = operate(ui);
 }
 
 clr.addEventListener('click', function(){
-    numOne.textContent = '';
-    op.textContent = '';
-    numTwo.textContent = '';
-    op_activated = false;
-    console.log(op_activated);
-    disTop.textContent = '';
-    
+    userInput.textContent = '';
+    results.textContent = '';
 })
 
 one.addEventListener('click', function(){
-    if(!op_activated){
-        numOne.textContent = numWrite(numOne.textContent, 1);
-    }
-    else{
-        numTwo.textContent = numWrite(numTwo.textContent, 1);
-    }
+    numWrite(1);
 })
 two.addEventListener('click', function(){
-    if(!op_activated){
-        numOne.textContent = numWrite(numOne.textContent, 2);
-    }
-    else{
-        numTwo.textContent = numWrite(numTwo.textContent, 2);
-    }
+    numWrite(2);
 })
 three.addEventListener('click', function(){
-    if(!op_activated){
-        numOne.textContent = numWrite(numOne.textContent, 3);
-    }
-    else{
-        numTwo.textContent = numWrite(numTwo.textContent, 3);
-    }
+    numWrite(3);
 })
 four.addEventListener('click', function(){
-    if(!op_activated){
-        numOne.textContent = numWrite(numOne.textContent, 4);
-    }
-    else{
-        numTwo.textContent = numWrite(numTwo.textContent, 4);
-    }
+    numWrite(4);
 })
 five.addEventListener('click', function(){
-    if(!op_activated){
-        numOne.textContent = numWrite(numOne.textContent, 5);
-    }
-    else{
-        numTwo.textContent = numWrite(numTwo.textContent, 5);
-    }
+    numWrite(5);
 })
 six.addEventListener('click', function(){
-    if(!op_activated){
-        numOne.textContent = numWrite(numOne.textContent, 6);
-    }
-    else{
-        numTwo.textContent = numWrite(numTwo.textContent, 6);
-    }
+    numWrite(6);
 })
 seven.addEventListener('click', function(){
-    if(!op_activated){
-        numOne.textContent = numWrite(numOne.textContent, 7);
-    }
-    else{
-        numTwo.textContent = numWrite(numTwo.textContent, 7);
-    }
+    numWrite(7);
 })
 eight.addEventListener('click', function(){
-    if(!op_activated){
-        numOne.textContent = numWrite(numOne.textContent, 8);
-    }
-    else{
-        numTwo.textContent = numWrite(numTwo.textContent, 8);
-    }
+    numWrite(8);
 })
 nine.addEventListener('click', function(){
-    if(!op_activated){
-        numOne.textContent = numWrite(numOne.textContent, 9);
-    }
-    else{
-        numTwo.textContent = numWrite(numTwo.textContent, 9);
-    }
+    numWrite(9);
 })
 zero.addEventListener('click', function(){
-    if(!op_activated){
-        numOne.textContent = numWrite(numOne.textContent, 0);
-    }
-    else{
-        numTwo.textContent = numWrite(numTwo.textContent, 0);
-    }
+    numWrite(0);
 })
 
 plus.addEventListener('click', function(){
-    if(!op_activated){
-        opActivate(` + `);
-    }
+    numWrite(" + ");
 } )
 minus.addEventListener('click', function(){
-    if(!op_activated){
-        opActivate(" - ");
-    }
+    numWrite(" - ");
 } )
 times.addEventListener('click', function(){
-    if(!op_activated){
-        opActivate(" × ");
-    }
+    numWrite(" × ");
 } )
 div.addEventListener('click', function(){
-    if(!op_activated){
-        opActivate(" ÷ ");
-    }
+    numWrite(" ÷ ");
 } )
 
 equals.addEventListener('click', function(){
-    disTop.textContent = `${numOne.textContent} ${op.textContent} ${numTwo.textContent}`
-    console.log(operate(numOne.textContent, op.textContent,numTwo.textContent));
-    numOne.textContent = operate(Number(numOne.textContent), op.textContent, Number(numTwo.textContent))
-    op.textContent = '';
-    numTwo.textContent = '';
-    op_activated = false;
+    results.textContent = operate(userInput.textContent);
+})
+
+point.addEventListener('click', function(){
+    numWrite(".");
 })
